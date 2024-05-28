0c775bbef4f4cc5f5dfb3604f3c87efc162fc293
Artem Tigranian <artem.tigranianp@gmail.com>
2024-05-28 20:22:34 +0700

    День недели

--- lab8/lab8.js
+++ lab8/lab8.js
@@ -32,3 +32,19 @@ function showMoreDate() {
 
     info.innerHTML = output;
 }
+
+function showDayOfWeek() {
+    let day = document.getElementById('day').value;
+    let month = document.getElementById('month').value - 1; // Месяцы в JS считаются с 0
+    let year = document.getElementById('year').value;
+
+    if(day && month >= 0 && year) {
+        let date = new Date(year, month, day);
+        const weekDays = ["Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"];
+        let dayOfWeek = weekDays[date.getDay()];
+
+        document.getElementById('day-of-week').innerHTML = 'День недели: ' + dayOfWeek;
+    } else {
+        document.getElementById('day-of-week').innerHTML = 'Пожалуйста, введите корректные значения даты.';
+    }
+}
