64aef4b2fd93afab8e338cd03db6a900ce489b58
Artem Tigranian <artem.tigranianp@gmail.com>
2024-05-28 19:35:23 +0700

    Вывод текущей даты

--- lab8/lab8.js
+++ lab8/lab8.js
@@ -11,3 +11,24 @@ function showDate() {
     output += 'Дата и время для греческой локали: ' + today.toLocaleString('el-GR');
     out.innerHTML = output;
 }
+
+function showMoreDate() {
+    let info = document.getElementById('more-date');
+    let today = new Date();
+
+    const weekDays = ["Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"];
+    const months = ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"];
+
+    let year = today.getFullYear();
+    let month = months[today.getMonth()];
+    let date = today.getDate();
+    let day = weekDays[today.getDay()];
+
+    let output = '';
+    output += 'Текущий год: ' + year + '<br>';
+    output += 'Текущий месяц: ' + month + '<br>';
+    output += 'Текущая дата: ' + date + '<br>';
+    output += 'День недели: ' + day;
+
+    info.innerHTML = output;
+}
