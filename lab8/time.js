a5a3ab9df3ac3cca3049671957586ae40eb49b28
Artem Tigranian <artem.tigranianp@gmail.com>
2024-05-28 22:38:52 +0700

    Аналоговые часы

--- lab8/time.js
+++ lab8/time.js
@@ -5,5 +5,9 @@ function showTime(){
 
     document.getElementById('time').innerHTML = currentTime;
     document.getElementById('seconds').innerHTML = currentSeconds;
+
+    let arrow = document.getElementById('arrow');
+    clock.style.transform = `rotate(${currentSeconds * 6}deg) translate(0, -50px)`;
 }
+
 setInterval(showTime, 1000);
